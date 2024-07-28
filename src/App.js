d880796228a4a18f4c4acd018215b6ddf9096b37
Muhammadrizo <muhammadrizo281@gmail.com>
2024-07-28 10:47:24 +0500

    zero

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import Activities from './components/Activities';
-import ClientsSwiper from './components/ClientsSwiper';
-import Contract from './components/Contract';
-import Footer from './components/Footer';
-import Header from './components/Header';
+
 import PopularProduct from './components/PopularProduct';
-import Recomendeyshon from './components/Recomendeyshon';
-import Sertificat from './components/Sertificat';
-import Widgets from './components/Widgets';
+
 import './style/App.css';
 
 function App() {
   return (
     <>
-      <Header />
-      <Widgets />
-      <Main />
-      <Info />
-      <Activities />
+    
       <PopularProduct />
-      <Recomendeyshon />
-      <ClientsSwiper />
-      <Contract />
-      <Sertificat />
-      <Footer />
+     
     </>
   );
 }
