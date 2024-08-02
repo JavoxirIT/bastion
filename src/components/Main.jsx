ded062fe5fe06f581632a4894838b698aa8522a9
JavoxirIt <javoxir5588@gmail.com>
2024-08-02 20:46:41 +0500

    Javoxir

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import css from "../style/Main.module.css"
+export default function Main({ mainCatd }) {
 
-export default function Main() {
+	console.log(mainCatd[0].title);
 	return (
-		<div >
-			Main
-		</div>
+		<div className={css.main} >
+			<div className={css.mainBackgound} >
+				<div className={css.mainContent} >
+					<h1 className={css.mainTitle} >Производим металлические <br />
+						изделия с <br />
+						<span className={css.mainTitleSpan} >2010</span> года <br />
+
+					</h1>
+					<p className={css.mainMiniTitle} >Умеем договариваться на <br /> выгодных для вас условиях!
+					</p>
+					<div className={css.mainMiniText} >
+						<span>Подробнее</span> <hr className={css.hr} />
+					</div>
+				</div>
+			</div>
+			<div style={{ marginTop: "-80px" }}>
+				<div className={css.mainFooter} >
+					{mainCatd.map((obj) =>
+						<div key={obj.id} className={css.mainFooterCard} >
+							<div className={css.cardImgBlock} >
+								<img className={css.cardImg} src={obj.img} alt="" />
+							</div>
+							<h5 className={css.cardTitle} >{obj.title}</h5>
+						</div>
+					)}
+				</div>
+			</div>
+		</div >
 	);
 }
--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -8,7 +8,7 @@ export default function PopularProduct() {
 	function mouseEnter(e) {
 
 		e.currentTarget.classList.add(style.PopularProduct_hover)
-		
+
 	}
 
 	function mouseLeave(e) {
@@ -22,7 +22,7 @@ export default function PopularProduct() {
 
 			<div className={style.PopularProduct_topBox}>
 
-			    <h1 className={style.PopularProduct_title}>Популярные товары</h1>
+				<h1 className={style.PopularProduct_title}>Популярные товары</h1>
 
 				<button className={style.PopularProduct_catalogButton}>Перейти в каталог</button>
 
@@ -30,27 +30,27 @@ export default function PopularProduct() {
 
 			<div className={style.PopularPoduct_gridBox}>
 
-			    {arrPopularProduct.map((product, index) => (
-    
-			    	<div key={index} className={style.PopularProduct_item} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+				{arrPopularProduct.map((product, index) => (
 
-						<img src={product.img} alt="img_src"  className={style.PopularProduct_itemImg}/>
+					<div key={index} className={style.PopularProduct_item} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+
+						<img src={product.img} alt="img_src" className={style.PopularProduct_itemImg} />
 
 						<div className={style.PopularProduct_itemText}>
 
-						    <span className ={style.PopularProduct_itemDate}>ГОСТ 14911-82</span>
-    
-						    <span className={style.PopularProduct_itemTitle}>Опора тавровая хомутовая </span>
+							<span className={style.PopularProduct_itemDate}>ГОСТ 14911-82</span>
+
+							<span className={style.PopularProduct_itemTitle}>Опора тавровая хомутовая </span>
 
 							<span className={style.PopularProduct_itemTx}>ТХ</span>
-    
-						    <span className={style.PopularProduct_itemPrice}>849.9 руб. </span>
+
+							<span className={style.PopularProduct_itemPrice}>849.9 руб. </span>
 
 						</div>
 
-			    	</div>
-    
-			    ))}
+					</div>
+
+				))}
 
 			</div>
 
